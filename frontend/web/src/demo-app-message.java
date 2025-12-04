21f57f3cc3db1aab4fdd19924ade6cbd7cedc9ab
1908008003RyanWelch <bela.u53882484@rambler.ru>
2025-12-04 16:25:02 +0800

    add IKMSVerifier.ts, configuration-download.js, user.yaml

---
+++ frontend/web/src/demo-app-message.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\demo-app-message.java
+// Generated at: 2025-10-12T06:55:00.021Z
---
+++ frontend/web/src/example-handler-queries/configuration-download.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function report() { return <div>Component</div>; }
